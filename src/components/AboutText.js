c947fcb4e4a534a0628ec8233fcff26334eb4b9c
Nathan Barrett <demeules.barrett@gmail.com>
2018-08-05 22:05:44 -0500

    make slight changes

--- src/components/AboutText.js
+++ src/components/AboutText.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import styled from 'react-emotion';
-import { css } from 'emotion';
-import Circle from '@fortawesome/fontawesome-free-solid/faCircle';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 
 const TextContainer = styled('div')({
   marginTop: 15,
@@ -31,49 +28,13 @@ const TextContainer = styled('div')({
   },
 });
 
-const icon = css({
-  fontSize: '10px',
-  marginLeft: 4,
-  marginRight: 4,
-  marginBottom: 1,
-  '@media (min-width: 500px)': {
-    marginBottom: 4,
-  },
-});
-const skillList = css({
-  fontSize: 14,
-});
-
-
 const AboutText = () => (
   <TextContainer>
-    <p>Hello! I&apos;m Nathan, a Portland based Web Developer. I love diving into a new code base and creating beautiful, usable applications. </p>
+    <p>
+      Hello! I&apos;m Nathan, a Portland based Web Developer. I love diving into a new code base and creating beautiful,
+      usable applications.
+    </p>
     <p> I strive to learn more and understand new and emerging technologies.</p>
-    <p>When I&apos;m not coding I love to read (both novels and comic books) as well as running around beautiful Portland.
-    </p>
-    <p className={skillList}>
-      <FontAwesomeIcon className={icon} color="#4183C4" icon={Circle} />
-        React
-        <FontAwesomeIcon className={icon} color="#4183C4" icon={Circle} />
-        React Native
-        <FontAwesomeIcon className={icon} color="#4183C4" icon={Circle} />
-        CSS/SCSS
-        <FontAwesomeIcon className={icon} color="#4183C4" icon={Circle} />
-      <br />
-      <FontAwesomeIcon className={icon} color="#4183C4" icon={Circle} />
-        Javascript/jQuery
-        <FontAwesomeIcon className={icon} color="#4183C4" icon={Circle} />
-        Git
-        <FontAwesomeIcon className={icon} color="#4183C4" icon={Circle} />
-      <br />
-      <FontAwesomeIcon className={icon} color="#4183C4" icon={Circle} />
-        ES6
-        <FontAwesomeIcon className={icon} color="#4183C4" icon={Circle} />
-          Angular 5
-        <FontAwesomeIcon className={icon} color="#4183C4" icon={Circle} />
-        NodeJS
-          <FontAwesomeIcon className={icon} color="#4183C4" icon={Circle} />
-    </p>
   </TextContainer>
 );
 
